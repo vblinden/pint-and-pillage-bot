b831394bd637dec787010bacc9e13a2df622c653
vblinden <vvanderlinden@swis.nl>
2020-01-24 13:56:24 +0100

    Add title of village and tile

--- index.js
+++ index.js
@@ -8,26 +8,22 @@ const pw = require('playwright');
             height: 1080
         }
     });
+
     const page = await context.newPage('https://pintandpillage.nl/login');
+    
+    await login(page);
+    await upgradeAllTiles(page, 0);
 
-    const username = await page.$('input.inputField:nth-child(1)');
-    await username.type('vblinden');
-
-    const password = await page.$('input.inputField:nth-child(2)');
-    await password.type('');
-
-    const submitButton = await page.$('.submitButton');
-    await submitButton.click();
-
-    await sleep(3000);
+    // console.log(await page.evaluate('localStorage.token'));
 
     const villages = await page.$$('.dropdown > div');
-
     for (let i = 1; i < villages.length; i++) {
         const villageSwitcher = await page.$('.villageSelector');
         await villageSwitcher.click();
 
-        await page.screenshot({ path: `village-${i}.png` });
+        const villageName = await page.$eval('.villageSelector', el => el.textContent);
+
+       //await page.screenshot({ path: `village-${i}.png` });
 
         await sleep(3000);
 
@@ -36,34 +32,54 @@ const pw = require('playwright');
 
         await sleep(2000);
 
-        console.log(`Checking village number: ${i}`)
+        console.log(`Checking village: ${villageName}`)
 
-        const tiles = await page.$$('.tile');
-        
-        for (let j = 0; j < tiles.length; j++) {
-            const tile = tiles[j];
-
-            await tile.click();
-            await sleep(25);
-
-            const levelUp = await page.$('text="Level Up"');
-            if (levelUp) {
-                await page.screenshot({ path: `village-${i}-tile-${j}.png` });
-                await sleep(10);
-                await levelUp.click()
-                console.log(`Level up tile ${j}...`);
-            }
-
-            const close = await page.$('.innerModalBox #modalButton');
-            if (close) {
-                await sleep(10);
-                close.click();
-            }
-        }
+        await upgradeAllTiles(page, i);
     }
 
     await browser.close();
 })();
+
+async function upgradeAllTiles(page, i) {
+    const tiles = await page.$$('.tile');
+        
+    for (let j = 0; j < tiles.length; j++) {
+        const tile = tiles[j];
+
+        await tile.click();
+        await sleep(25);
+
+        const levelUp = await page.$('//button[text()="Level Up"][not(@disabled)]');
+        if (levelUp) {
+            //await page.screenshot({ path: `village-${i}-tile-${j}.png` });
+            await sleep(10);
+
+            await levelUp.click()
+
+            const title = await page.$eval('h1', el => el.textContent);
+
+            console.log(`Level up ${title}...`);
+        }
+
+        const close = await page.$('.innerModalBox #modalButton');
+        if (close) {
+            await sleep(10);
+            close.click();
+        }
+    }
+}
+
+async function login(page) {
+    const username = await page.$('input.inputField:nth-child(1)');
+    await username.type('vblinden');
+
+    const password = await page.$('input.inputField:nth-child(2)');
+    await password.type('');
+
+    const submitButton = await page.$('.submitButton');
+    await submitButton.click();
+    await sleep(3000);
+}
 
 function sleep(millis) {
     return new Promise(resolve => setTimeout(resolve, millis));
